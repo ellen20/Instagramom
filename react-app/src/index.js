82b80aa3a3f964a15624dd9d15ec39c2dac285ec
Jingling Jin <jljin0524@gmail.com>
2022-06-16 10:23:40 -0500

    add modal styling

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -6,18 +6,21 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 import { NavModalProvider } from './context/NavModal';
+import { PostModalProvider } from './context/PostModal';
 
 const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <NavModalProvider>
-      <ModalProvider>
-        <Provider store={store}>
-            <App />
-          </Provider>
-      </ModalProvider>
-    </NavModalProvider>
+    <PostModalProvider>
+      <NavModalProvider>
+        <ModalProvider>
+          <Provider store={store}>
+              <App />
+            </Provider>
+        </ModalProvider>
+      </NavModalProvider>
+    </PostModalProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
